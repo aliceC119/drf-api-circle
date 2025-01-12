9e51a5fd84f2d942235670a23fa690e98bf19921
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-12 11:01:44 +0000

    added the CSRF_TRUSTED_ORIGINS setting.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -98,7 +98,10 @@ DATABASES = {
     }
 }
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
