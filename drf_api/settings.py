72943ae91ac693754bc482108414ee11b64b75eb
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-18 15:43:30 +0000

    updated the heruko link in the settings.py.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -70,7 +70,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://drf-api-circle-320cd30d1665.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'https://drf-api-circle-320cd30d1665.herokuapp.com']
 
 
 # Application definition
