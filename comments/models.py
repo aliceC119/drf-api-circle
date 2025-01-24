24d3097fcbd3208862009ff155dec0f23a866fcd
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-24 08:50:16 +0000

    added a commented out code for App 'comments'

--- comments/models.py
+++ comments/models.py
@@ -9,7 +9,7 @@ class Comment(models.Model):
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    video_post = models.ForeignKey(VideoPost, on_delete=models.CASCADE, null=True, blank=True)
+    #video_post = models.ForeignKey(VideoPost, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -19,5 +19,24 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+
+
+#class VideoPostComment(models.Model):
+#    """
+#   Comment model, related to User and Post
+#    """
+#    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+#    
+#    video_post = models.ForeignKey(VideoPost, on_delete=models.CASCADE, null=True, blank=True)
+#    created_at = models.DateTimeField(auto_now_add=True)
+#    updated_at = models.DateTimeField(auto_now=True)
+#   content = models.TextField()
+
+#    class Meta:
+#       ordering = ['-created_at']
+
+#    def __str__(self):
+#       return self.content
+
 
 
--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
+#from .models import VideoPostComment
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -29,7 +30,7 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'video_post', 'created_at', 'updated_at', 'content'
+            'post', 'created_at', 'updated_at', 'content'
         ]
 
 class CommentDetailSerializer(CommentSerializer):
@@ -38,3 +39,40 @@ class CommentDetailSerializer(CommentSerializer):
     Post is a read only field so that we dont have to set it on each update
     """
     post = serializers.ReadOnlyField(source='post.id')
+
+
+#class VideoPostCommentSerializer(serializers.ModelSerializer):
+#    """
+#    Serializer for the Comment model
+#    Adds three extra fields when returning a list of Comment instances
+#   """
+#    owner = serializers.ReadOnlyField(source='owner.username')
+#    is_owner = serializers.SerializerMethodField()
+#   profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+#   profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+#    created_at = serializers.SerializerMethodField()
+#    updated_at = serializers.SerializerMethodField()
+
+#    def get_is_owner(self, obj):
+#        request = self.context['request']
+#        return request.user == obj.owner
+    
+#    def get_created_at(self, obj):
+#       return naturaltime(obj.created_at)
+
+#    def get_updated_at(self, obj):
+#        return naturaltime(obj.updated_at)
+
+#    class Meta:
+#        model = Comment
+#        fields = [
+#            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+#            'video_post', 'created_at', 'updated_at', 'content'
+#        ]
+
+#class VideoPostCommentDetailSerializer(CommentSerializer):
+#    """
+#    Serializer for the Comment model used in Detail view
+#    Post is a read only field so that we dont have to set it on each update
+#    """
+#    video_post = serializers.ReadOnlyField(source='video_post.id')
