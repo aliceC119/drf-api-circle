5d4bfd74dda8ebf4f692e7afb27f3cb3fb08efc3
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-24 09:01:26 +0000

    Added two commented out path for App comments.

--- comments/urls.py
+++ comments/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from comments import views
 
 urlpatterns = [
-    path('comments/', views.CommentList.as_view()),
-    path('comments/<int:pk>/', views.CommentDetail.as_view())
+    path('comments/posts/', views.CommentList.as_view()),
+    path('comments/posts/<int:pk>/', views.CommentDetail.as_view()),
+#    path('comments/videoposts/', views.CommentList.as_view()),
+#    path('comments/videoposts/<int:pk>/', views.CommentDetail.as_view())
 ]
