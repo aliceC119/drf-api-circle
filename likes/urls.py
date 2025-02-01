eac80d2c2e13fac42bc1a73ee580f6b2df54ebf4
Alice Chan <alicebay10@yahoo.co.uk>
2025-02-01 12:04:18 +0000

    added 'video-post-like-detail' to the likes/videoposts/<int:pk>/ path.

--- likes/urls.py
+++ likes/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('likes/posts/', views.LikePost.as_view(), name='post-like-list'),  # List all post likes
     path('likes/posts/<int:pk>/', views.LikePostDetail.as_view(), name='like-post'),
     path('likes/videoposts/', views.VideoPostLikeList.as_view(), name='video-post-like-list'),  # List all video post likes
-    path('likes/videoposts/<int:pk>/', views.VideoPostLikeDetail.as_view(), name='like-video-post'),
+    path('likes/videoposts/<int:pk>/', views.VideoPostLikeDetail.as_view(), name='video-post-like-detail'),
     
 ]
